59a7117a30bffe0cfb6eff475e4a453eafc5a2a8
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-03-12 09:28:57 +0900

    models blog.rb作成

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ Things you may want to cover:
 *　users controller作成
 *　users new.html.erb
 *　users show.html.erb作成
-*
+*　models user.rb作成
 *
 *
 *
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,8 @@
 class Blog < ApplicationRecord
+    belongs_to :user, optional: true 
+    validates :title, presence: true
+    validates :content, presence: true
+    has_many :favorites, dependent: :destroy
+    has_many :favorite_users, through: :favorites, source: :user
 end
+  

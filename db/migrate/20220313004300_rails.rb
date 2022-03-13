ee50a8d78db473dba4d30822384e9a3053f3f57e
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-03-13 09:45:44 +0900

    Feed table属性変更

--- db/migrate/20220313004300_rails.rb
+++ db/migrate/20220313004300_rails.rb
@@ -1,6 +1,6 @@
 class Rails < ActiveRecord::Migration[6.0]
   def change
-    t.bigint "user_id", null: false
-    t.bigint "blog_id", null: false
+    t.bigint "user_id", null: true
+    t.bigint "blog_id", null: true
   end
 end

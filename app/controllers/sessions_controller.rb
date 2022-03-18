b88cf54055843afa5c6a6259ce7546465f04f01b
satokofuwa <96944825+satokofuwa@users.noreply.github.com>
2022-03-18 11:18:24 +0900

    ログイン処理をapplication.htmlに追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,19 @@ class SessionsController < ApplicationController
     end
     
     def create
-      user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
-        session[:user_id] = user.id
-        redirect_to user_path(user.id)
+      if logged_in?
+        flash[:notice] = 'Already logged in'
+        redirect_to new_session_path
       else
-        flash.now[:danger] = 'ログインに失敗しました'
-        render :new
+
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+          session[:user_id] = user.id
+          redirect_to user_path(user.id)
+        else
+          flash.now[:danger] = 'ログインに失敗しました'
+          render :new
+        end
       end
     end
 

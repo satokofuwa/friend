2fb914a94ab2f78457f29d278a770d3195566ff7
satokofuwa <sato.fuwa@nifty.com>
2022-05-01 21:22:50 +0900

    migration file 修正

--- db/migrate/20220312135138_add_img_to_blogs.rb
+++ db/migrate/20220312135138_add_img_to_blogs.rb
@@ -1,6 +1,6 @@
 class AddImgToBlogs < ActiveRecord::Migration[6.0]
   def change
     add_column :blogs, :img, :string
-    t.references :user, null: false, foreign_key: true
+    add_reference :blogs, :user, foreign_key: true, null: false
   end
 end
